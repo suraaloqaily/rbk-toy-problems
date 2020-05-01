2525f651651966041800fc303be1c21b8ccec364
suraaloqaily <aloqailysura@gmail.com>
2020-05-01 09:47:18 +0300

    W4D6 solves

--- W4D6/W4D6.js
+++ W4D6/W4D6.js
@@ -11,3 +11,10 @@ Another example could be:
 
 { name: "Gelato", countryOfOrigin: "Italy", flavor: "Chocolate Chip" }
 */
+function favoriteDessert(){
+	return obj={
+		 name: "cake",
+		  ovenTemp: 150,
+		   prepTime: "50 minutes" 
+	}
+}
