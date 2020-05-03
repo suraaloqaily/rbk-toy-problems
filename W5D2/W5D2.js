17393675af016299707ae756aa70afc1b2990003
suraaloqaily <aloqailysura@gmail.com>
2020-05-03 09:45:43 +0300

    W5D2 solved.

--- W5D2/W5D2.js
+++ W5D2/W5D2.js
@@ -1,3 +1,42 @@
+function each(coll, f) {
+  if (Array.isArray(coll)) {
+    for (var i = 0; i < coll.length; i++) {
+      f(coll[i], i);
+    }
+  } else {
+    for (var key in coll) {
+      f(coll[key], key);
+    }
+  }
+}
+
+function filter(array, predicate) {
+  var acc = [];
+  each(array, function(element, i) {
+    if (predicate(element, i)) {
+      acc.push(element);
+    }
+  });
+  return acc;
+}
+
+function map(array, func) {
+  var acc = [];
+  each(array, function(element, i) {
+    acc.push(func(element, i));
+  });
+  return acc;
+}
+function reduce(array, f, acc) { 
+ if (acc === undefined) { 
+   acc = array[0]; 
+   array = array.slice(1); 
+ } 
+ each(array, function(element, i) { 
+   acc = f(acc, element, i); 
+ }); 
+ return acc; 
+}
 /*
 1-Write a function called tallEnoughToRide  that takes an array of people objects, 
 and returns a an array of names of people who are greater than or equal to 48 inches in height.
@@ -37,6 +76,17 @@ I.e. If someone called your function with 10 objects
 
 // your answer is here
 
+function tallEnoughToRide(arrayOfObjects){
+  var output=  map(arrayOfObjects ,function (object){
+    if(object.heightInInches >=48){
+      return object.name
+    }
+  })
+return filter(output,function(element){
+  return element !==undefined
+})
+}
+
 /*
 2-Working off of the same data structure as tallEnoughToRide, 
 write a function called tallestPerson that takes  an array of people objects as well, 
@@ -51,3 +101,46 @@ tallestPerson(groupA); //"Kiana at 55 inches"
 */
 
 // your answer is here
+
+function tallestPerson(arrayOfObjects){
+  var arrayOfLengths=map(arrayOfObjects,function(object){
+return object.heightInInches
+
+
+  })
+var maxNumber= reduce (arrayOfLengths ,function (max,element){
+    if(max<element){
+      max=element
+    }
+    return max
+  })
+
+  var output= map(arrayOfObjects,function(object){
+
+    if(object.heightInInches === maxNumber)
+     return object.name + " at " + maxNumber + " inches "
+
+
+  })
+  var answer= filter(output,function(element){
+  return element !==undefined
+})
+  return answer[0]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
